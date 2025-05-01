7601d9d637af484f4b1a04414addbdee21b9d0c7
Md Shahdat Hossain <166033634+MShahdat@users.noreply.github.com>
2025-05-02 02:52:18 +0600

    Update AboutUs.jsx

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -10,7 +10,6 @@ const AboutUs = () => {
             <NavBar></NavBar>
             <h1>this is a about page</h1>
             <p>this is the paragraph tag</p>
-            <p>done</p>
             <p>ok</p>
             <Footer></Footer>
         </div>
